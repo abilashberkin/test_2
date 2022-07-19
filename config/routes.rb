0f8d5954f679549cf62c04fa7a385071bbefc146
Abilash <abilash.berkin@kreatio.com>
2022-07-19 14:01:47 +0530

    Initial commit and add root route

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :players
+  root 'home#index'
+  get 'about', to: 'pages#about'
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
